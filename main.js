a2b626e7f69c5af8228c2685404352e0fff5533d
AppVeyor <ci@appveyor.com>
2024-03-29 08:15:22 +0000

    c21f372

---
+++ main.js
@@ -0,0 +1,36 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+
+;// CONCATENATED MODULE: ./src/js/tooltip.js
+class Tooltip {
+  constructor(tooltip) {
+    this.tooltip = tooltip;
+  }
+  onButton(e) {
+    e.preventDefault();
+    const popover = this.tooltip.querySelector(".tooltip");
+    if (popover.classList.contains("active")) {
+      popover.classList.remove("active");
+    } else {
+      popover.classList.add("active");
+    }
+  }
+  bind() {
+    const button = this.tooltip.querySelector(".btn");
+    button.addEventListener("click", e => this.onButton(e));
+  }
+}
+;// CONCATENATED MODULE: ./src/js/app.js
+// TODO: write code here
+
+const container = document.querySelector(".container");
+const tooltip = new Tooltip(container);
+tooltip.bind();
+;// CONCATENATED MODULE: ./src/index.js
+
+
+
+// TODO: write your code in app.js
+/******/ })()
+;
